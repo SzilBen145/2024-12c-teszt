201a3f18491c5192b5441b75bd21e644146b0234
SzilBen145 <2021145@dszcberegszaszi.hu>
2024-10-09 11:59:19 +0200

    ok

---
+++ KorHenger-Teszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KorHenger-Teszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KorHenger-Teszt")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ced965d0-3c5a-4de0-a45b-8e9095ce73ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KorHenger/Kor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KorHenger
+{
+    class Kor
+    {
+        private double sugar, kerület, terület;
+
+        public Kor()
+        {
+
+        }
+
+        public Kor(double r)
+        {
+            setDatas(r);
+        }
+
+        private double kalkKerület(double r)
+        {
+            return 2 * r * Math.PI;
+        }
+
+        private double kalkTerület(double r)
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+        
+        public double GetSugár()
+        {
+            return this.sugar;
+        }
+
+        public double GetKerület()
+        {
+            return this.kerület;
+        }
+
+        public double GetTerület()
+        {
+            return this.terület;
+        }
+
+        public void SetSugar(double r)
+        {
+            setDatas(r);
+        }
+
+        private void setDatas(double r)
+        {
+            this.sugar = r;
+
+            this.kerület = kalkKerület(r);
+
+            this.terület = kalkTerület(r);
+        }
+
+
+
+    }
+}
